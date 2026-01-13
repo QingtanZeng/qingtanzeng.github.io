a6375b8beb0524956665509d11c58dc0b376a440
QingtanZeng <zengqt.e@gmail.com>
2026-01-13 00:54:02 +0000

    Deploying to gh-pages from @ QingtanZeng/qingtanzeng.github.io@9d4c3bec0f753b7345cf8145dfb36e3f60250005 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -431,9 +431,9 @@ ninja.data = [{
           description: "Homogeneous Self-dual Embedding(HSDE) IPM, Homogeneous Embedding(HE) IPM, HE Douglas-Rachford Splitting(DRS)",
           section: "Projects",handler: () => {
               window.location.href = "/projects/3_project/";
-            },},{id: "projects-notes-on-constrained-stein-variational-inference-for-global-optimization",
-          title: 'Notes on Constrained Stein Variational Inference for Global Optimization',
-          description: "MultiModal, NonConvex-NonConnected-NonDifferentiable, Hard Constraints, GPU Acceleration.",
+            },},{id: "projects-",
+          title: '',
+          description: "",
           section: "Projects",handler: () => {
               window.location.href = "/projects/4_project/";
             },},{id: "projects-inverter-modulation",
@@ -441,9 +441,9 @@ ninja.data = [{
           description: "Report on Inverter Modulation",
           section: "Projects",handler: () => {
               window.location.href = "/projects/5_project/";
-            },},{id: "projects-project-6",
-          title: 'project 6',
-          description: "a project with no image",
+            },},{id: "projects-inverter-modulation",
+          title: 'Inverter Modulation',
+          description: "Report on Pulse Heating",
           section: "Projects",handler: () => {
               window.location.href = "/projects/6_project/";
             },},{id: "projects-project-7",
